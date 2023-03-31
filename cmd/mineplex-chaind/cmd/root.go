b4c86b1330e2fd7ef75ab431fa04d3dbbd7d8e2e
mineplexio <info@mineplex.io>
2023-03-31 12:52:44 +0300

    Set default min gas prices

--- cmd/mineplex-chaind/cmd/root.go
+++ cmd/mineplex-chaind/cmd/root.go
@@ -342,7 +342,7 @@ func initAppConfig() (string, interface{}) {
 	//   own app.toml to override, or use this default value.
 	//
 	// In simapp, we set the min gas prices to 0.
-	srvCfg.MinGasPrices = "0stake"
+	srvCfg.MinGasPrices = "10000000000000mine2"
 
 	customAppConfig := CustomAppConfig{
 		Config: *srvCfg,
