2bfb83808a34ccc028158b50e982a9c206517ec0
mineplexio <info@mineplex.io>
2023-03-31 12:52:55 +0300

    Fix address prefix in explorer-api

--- cmd/explorer-api/main.go
+++ cmd/explorer-api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ignite/cli/ignite/pkg/clictx"
 	"github.com/ignite/cli/ignite/pkg/cosmosclient"
 	"github.com/ignite/cli/ignite/pkg/cosmostxcollector/adapter/postgres"
@@ -18,25 +17,7 @@ const (
 	rpcAddr = "http://0.0.0.0:26657"
 )
 
-func initSDKConfig() {
-	// Set prefixes
-	accountPubKeyPrefix := app.AccountAddressPrefix + "pub"
-	validatorAddressPrefix := app.AccountAddressPrefix + "valoper"
-	validatorPubKeyPrefix := app.AccountAddressPrefix + "valoperpub"
-	consNodeAddressPrefix := app.AccountAddressPrefix + "valcons"
-	consNodePubKeyPrefix := app.AccountAddressPrefix + "valconspub"
-
-	// Set and seal config
-	config := sdk.GetConfig()
-	config.SetBech32PrefixForAccount(app.AccountAddressPrefix, accountPubKeyPrefix)
-	config.SetBech32PrefixForValidator(validatorAddressPrefix, validatorPubKeyPrefix)
-	config.SetBech32PrefixForConsensusNode(consNodeAddressPrefix, consNodePubKeyPrefix)
-	config.Seal()
-}
-
 func main() {
-	//initSDKConfig()
-
 	ctx := clictx.From(context.Background())
 
 	// Init an adapter for a local PostgreSQL database running with the default values
@@ -47,7 +28,7 @@ func main() {
 	}
 
 	// Init the Cosmos client
-	client, err := cosmosclient.New(ctx, cosmosclient.WithNodeAddress(rpcAddr))
+	client, err := cosmosclient.New(ctx, cosmosclient.WithNodeAddress(rpcAddr), cosmosclient.WithAddressPrefix(app.AccountAddressPrefix))
 	if err != nil {
 		panic(err)
 	}
