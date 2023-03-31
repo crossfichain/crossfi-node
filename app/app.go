ac5e266080a73188a37276ef069a8ae68f0955cf
mineplexio <info@mineplex.io>
2023-03-31 12:52:59 +0300

    Add treasury module perms

--- app/app.go
+++ app/app.go
@@ -184,6 +184,7 @@ var (
 		stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
 		govtypes.ModuleName:            {authtypes.Burner},
 		ibctransfertypes.ModuleName:    {authtypes.Minter, authtypes.Burner},
+		treasurymoduletypes.ModuleName: {authtypes.Minter, authtypes.Burner},
 		// this line is used by starport scaffolding # stargate/app/maccPerms
 	}
 )
