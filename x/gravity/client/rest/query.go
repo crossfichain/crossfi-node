e6f2c34f1d7e839a5a543af13ae540e44e290521
mineplexio <info@mineplex.io>
2023-07-12 12:06:14 +0300

    "Refactor client error response handling in gravity module"
    This commit refactors the handling of error responses in certain areas of the "gravity" module of the Cosmos-SDK. Rather than relying on the rest.WriteErrorResponse and rest.PostProcessResponse methods from the Cosmos-SDK's REST package, we've introduced equivalent functions directly in our module. These new methods were introduced in query.go file.
    
    This allows us to have more control over the error response behavior directly within our module, and potentially make modifications that may not be fit for contribution back to the larger Cosmos-SDK project. It also adds the functionality to wrap responses with the height, providing more detailed information to the client. A notable addition here is the encoding/json package, used to process JSON messages in responses.
    
    Remember this makes part of our efforts to ensure orderly and accurate error reporting, crucial for effective debugging and user feedback.

--- x/gravity/client/rest/query.go
+++ x/gravity/client/rest/query.go
@@ -1,11 +1,12 @@
 package rest
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/cosmos/cosmos-sdk/codec/legacy"
 	"net/http"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/gorilla/mux"
 
 	"github.com/mineplexio/mineplex-2-node/x/gravity/types"
@@ -18,17 +19,17 @@ func getValsetRequestHandler(cliCtx client.Context, storeName string) http.Handl
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/valsetRequest/%s", storeName, nonce))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset not found")
 			return
 		}
 
 		var out types.Valset
 		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -41,17 +42,17 @@ func batchByNonceHandler(cliCtx client.Context, storeName string) http.HandlerFu
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/batch/%s/%s", storeName, nonce, denom))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset not found")
 			return
 		}
 
 		var out types.OutgoingTxBatch
 		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -61,15 +62,15 @@ func lastBatchesHandler(cliCtx client.Context, storeName string) http.HandlerFun
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/lastBatches", storeName))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset not found")
 			return
 		}
 
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -81,15 +82,15 @@ func allValsetConfirmsHandler(cliCtx client.Context, storeName string) http.Hand
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/valsetConfirms/%s", storeName, nonce))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset confirms not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset confirms not found")
 			return
 		}
 
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -102,15 +103,15 @@ func allBatchConfirmsHandler(cliCtx client.Context, storeName string) http.Handl
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/batchConfirms/%s/%s", storeName, nonce, denom))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset confirms not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset confirms not found")
 			return
 		}
 
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -118,15 +119,15 @@ func lastValsetRequestsHandler(cliCtx client.Context, storeName string) http.Han
 	return func(w http.ResponseWriter, r *http.Request) {
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/lastValsetRequests", storeName))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "valset requests not found")
+			WriteErrorResponse(w, http.StatusNotFound, "valset requests not found")
 			return
 		}
 
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -137,17 +138,17 @@ func lastValsetRequestsByAddressHandler(cliCtx client.Context, storeName string)
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/lastPendingValsetRequest/%s", storeName, operatorAddr))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "no pending valset requests found")
+			WriteErrorResponse(w, http.StatusNotFound, "no pending valset requests found")
 			return
 		}
 
 		var out types.Valset
 		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -158,17 +159,17 @@ func lastBatchesByAddressHandler(cliCtx client.Context, storeName string) http.H
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/lastPendingBatchRequest/%s", storeName, operatorAddr))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+			WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 		if len(res) == 0 {
-			rest.WriteErrorResponse(w, http.StatusNotFound, "no pending valset requests found")
+			WriteErrorResponse(w, http.StatusNotFound, "no pending valset requests found")
 			return
 		}
 
 		var out types.OutgoingTxBatch
 		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -176,12 +177,12 @@ func currentValsetHandler(cliCtx client.Context, storeName string) http.HandlerF
 	return func(w http.ResponseWriter, r *http.Request) {
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/currentValset", storeName))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
 		var out types.Valset
 		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -192,10 +193,10 @@ func denomToERC20Handler(cliCtx client.Context, storeName string) http.HandlerFu
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/DenomToERC20/%s", storeName, denom))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
 	}
 }
 
@@ -206,9 +207,94 @@ func ERC20ToDenomHandler(cliCtx client.Context, storeName string) http.HandlerFu
 
 		res, height, err := cliCtx.Query(fmt.Sprintf("custom/%s/ERC20ToDenom/%s", storeName, ERC20))
 		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
+			WriteErrorResponse(w, http.StatusInternalServerError, err.Error())
 			return
 		}
-		rest.PostProcessResponse(w, cliCtx.WithHeight(height), res)
+		PostProcessResponse(w, cliCtx.WithHeight(height), res)
+	}
+}
+
+func WriteErrorResponse(w http.ResponseWriter, status int, err string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, _ = w.Write(legacy.Cdc.MustMarshalJSON(NewErrorResponse(0, err)))
+}
+
+type ErrorResponse struct {
+	Code  int    `json:"code,omitempty"`
+	Error string `json:"error"`
+}
+
+// NewErrorResponse creates a new ErrorResponse instance.
+func NewErrorResponse(code int, err string) ErrorResponse {
+	return ErrorResponse{Code: code, Error: err}
+}
+
+// PostProcessResponse performs post processing for a REST response. The result
+// returned to clients will contain two fields, the height at which the resource
+// was queried at and the original result.
+func PostProcessResponse(w http.ResponseWriter, ctx client.Context, resp interface{}) {
+	var (
+		result []byte
+		err    error
+	)
+
+	if ctx.Height < 0 {
+		WriteErrorResponse(w, http.StatusInternalServerError, fmt.Errorf("negative height in response").Error())
+		return
+	}
+
+	// LegacyAmino used intentionally for REST
+	marshaler := ctx.LegacyAmino
+
+	switch res := resp.(type) {
+	case []byte:
+		result = res
+
+	default:
+		result, err = marshaler.MarshalJSON(resp)
+		if CheckInternalServerError(w, err) {
+			return
+		}
+	}
+
+	wrappedResp := NewResponseWithHeight(ctx.Height, result)
+
+	output, err := marshaler.MarshalJSON(wrappedResp)
+	if CheckInternalServerError(w, err) {
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, _ = w.Write(output)
+}
+
+// CheckInternalServerError attaches an error message to an HTTP 500 INTERNAL SERVER ERROR response.
+// Returns false when err is nil; it returns true otherwise.
+func CheckInternalServerError(w http.ResponseWriter, err error) bool {
+	return CheckError(w, http.StatusInternalServerError, err)
+}
+
+func CheckError(w http.ResponseWriter, status int, err error) bool {
+	if err != nil {
+		WriteErrorResponse(w, status, err.Error())
+		return true
+	}
+
+	return false
+}
+
+// ResponseWithHeight defines a response object type that wraps an original
+// response with a height.
+type ResponseWithHeight struct {
+	Height int64           `json:"height"`
+	Result json.RawMessage `json:"result"`
+}
+
+// NewResponseWithHeight creates a new ResponseWithHeight instance
+func NewResponseWithHeight(height int64, result json.RawMessage) ResponseWithHeight {
+	return ResponseWithHeight{
+		Height: height,
+		Result: result,
 	}
 }
