2922bac09d270563c8dbc5ae3e7c9a9ff392dbed
crossfidev <info@mineplex.io>
2025-03-12 10:24:18 +0300

    decon upgrade – adjust validator stakes

---
+++ app/upgrades/decon/adjust_test.go
@@ -0,0 +1,45 @@
+package decon
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestAdjustValidatorStakes(t *testing.T) {
+	maxPercent := int64(40)
+
+	// Create validators with varying token amounts
+	vals := stakingtypes.Validators{
+		{OperatorAddress: "val1", Tokens: sdk.NewInt(300)}, // Exceeds limit
+		{OperatorAddress: "val2", Tokens: sdk.NewInt(150)}, // Below limit
+		{OperatorAddress: "val3", Tokens: sdk.NewInt(250)}, // Exceeds limit
+	}
+
+	totalBonded := sdk.NewInt(0)
+	for _, val := range vals {
+		totalBonded = totalBonded.Add(val.Tokens)
+	}
+
+	// Adjust the stakes
+	adjustedVals := adjustValidatorStakes(vals, maxPercent)
+
+	newTotalBonded := sdk.NewInt(0)
+	for _, amount := range adjustedVals {
+		newTotalBonded = newTotalBonded.Add(amount)
+	}
+
+	newMaxStake := newTotalBonded.MulRaw(maxPercent).QuoRaw(100)
+
+	println("Total bonded", totalBonded.String())
+	println("New total bonded", newTotalBonded.String())
+	println("Subtracted", totalBonded.Sub(newTotalBonded).String())
+
+	// Assertions
+	require.Len(t, adjustedVals, len(vals), "Number of validators should remain the same")
+	for val, tokens := range adjustedVals {
+		println(val, tokens.String())
+		require.True(t, tokens.LTE(newMaxStake), "Validator stake should not exceed % of total bonded tokens")
+	}
+}
--- app/upgrades/decon/upgrades.go
+++ app/upgrades/decon/upgrades.go
@@ -1,9 +1,11 @@
 package decon
 
 import (
+	"cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 )
 
@@ -33,6 +35,80 @@ func CreateUpgradeHandler(
 			}
 		}
 
+		adjustedStakes := adjustValidatorStakes(stakingKeeper.GetAllValidators(ctx), 10)
+
+		for _, val := range stakingKeeper.GetAllValidators(ctx) {
+			adjusted := adjustedStakes[val.OperatorAddress]
+			if val.Tokens.GT(adjusted) {
+				excessTokens := val.Tokens.Sub(adjusted)
+				fractionToUnbond := sdk.NewDecFromInt(excessTokens).Quo(sdk.NewDecFromInt(val.Tokens))
+				logger.Info(
+					"validator exceeds 10% of total stake; unbonding excess tokens",
+					"validator", val.GetOperator(), "excess", excessTokens, "fraction", fractionToUnbond,
+				)
+
+				delegations := stakingKeeper.GetValidatorDelegations(ctx, val.GetOperator())
+				for _, delegation := range delegations {
+					unbondSharesAmount := delegation.Shares.Mul(fractionToUnbond)
+					unbondAmount := val.TokensFromShares(unbondSharesAmount)
+					if unbondAmount.TruncateInt().IsZero() {
+						continue
+					}
+
+					if _, err := stakingKeeper.Undelegate(
+						ctx, sdk.MustAccAddressFromBech32(delegation.DelegatorAddress), val.GetOperator(),
+						unbondSharesAmount,
+					); err != nil {
+						return nil, err
+					}
+					logger.Info(
+						"undelegated tokens from delegator",
+						"delegator", delegation.DelegatorAddress,
+						"validator", val.GetOperator(),
+						"shares", unbondSharesAmount,
+						"amount", unbondAmount,
+					)
+				}
+			}
+		}
+
 		return vm, nil
 	}
+}
+
+func adjustValidatorStakes(vals stakingtypes.Validators, maxPercent int64) map[string]math.Int {
+	if 100/int64(len(vals)) > maxPercent {
+		maxPercent = int64(100/len(vals)) + 1
+		println("maxPercent is too low, adjusting to", maxPercent)
+	}
+
+	totalBonded := sdk.NewInt(0)
+	for _, val := range vals {
+		totalBonded = totalBonded.Add(val.Tokens)
+	}
+
+	for i := 0; i < 100000; i++ {
+		maxStake := totalBonded.MulRaw(maxPercent).QuoRaw(100)
+		hasOverflow := false
+
+		for i, val := range vals {
+			if val.Tokens.GT(maxStake) {
+				sub := vals[i].Tokens.QuoRaw(100)
+				vals[i].Tokens = vals[i].Tokens.Sub(sub)
+				totalBonded = totalBonded.Sub(sub)
+				hasOverflow = true
+			}
+		}
+
+		if !hasOverflow {
+			break
+		}
+	}
+
+	valsMap := make(map[string]math.Int)
+	for _, val := range vals {
+		valsMap[val.OperatorAddress] = val.Tokens
+	}
+
+	return valsMap
 }
